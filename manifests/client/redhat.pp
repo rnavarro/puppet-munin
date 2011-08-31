1ab0dc3a9972fd78812090bd8e13c8897b025bb2
Ewoud Kohl van Wijngaarden <e.kohlvanwijngaarden@oxilion.nl>
2011-08-31 14:03:42 +0200

    Use the correct module name

--- manifests/client/redhat.pp
+++ manifests/client/redhat.pp
@@ -25,5 +25,5 @@ class munin::client::redhat
 	munin::register { $fqdn: }
 
 	# workaround bug in munin_node_configure
-	plugin { "postfix_mailvolume": ensure => absent }
+	munin::plugin { "postfix_mailvolume": ensure => absent }
 }
